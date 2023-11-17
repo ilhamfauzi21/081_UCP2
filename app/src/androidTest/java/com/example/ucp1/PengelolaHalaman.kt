ade650463b5a782eb6fc9db85a16f7df6cb9433a
Ilham Fauzi <ilham.fauzi.ft21@mail.umy.ac.id>
2023-11-17 14:54:56 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/example/ucp1/PengelolaHalaman.kt
@@ -0,0 +1,35 @@
+package com.example.ucp1
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun EsJumboAppBar(
+    bisaNavigasiBack: Boolean,
+    navigasiUp: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    TopAppBar(title = { Text(stringResource(id = R.string.app_name)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer),
+        modifier = modifier,
+        navigationIcon = {
+            if (bisaNavigasiBack){
+                IconButton(onClick = navigasiUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
